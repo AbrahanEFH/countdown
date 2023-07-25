e9ffd2595fa717a4edfa79fa023cf4828e0d4051
abrahan <abrahandev@gmail.com>
2023-07-25 14:14:11 +0200

    Contador terminador

---
+++ README.md
@@ -0,0 +1,1 @@
+## Contador en Javascript vanilla y css
--- script.js
+++ script.js
@@ -1,5 +1,34 @@
+const daysEl =document.querySelector('#days')
+const hoursEl =document.querySelector('#hours')
+const minsEl =document.querySelector('#mins')
+const secondsEl =document.querySelector('#seconds')
 
-const buton = document.querySelector('#boton')
-buton.addEventListener('click', () => {
-    console.log('clik en el boton')
-})
+
+
+const vacations = '3 Aug 2023';
+
+function countdown() {
+    const vacationsDate = new Date(vacations)
+    const currentDate = new Date();
+   
+    const totalSeconds = (vacationsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+    
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minsEl.innerHTML = mins;
+    secondsEl.innerHTML = seconds;
+
+
+    console.log(days, hours, mins, seconds);
+}
+
+
+// initial call
+countdown();
+
+setInterval(countdown, 1000);
